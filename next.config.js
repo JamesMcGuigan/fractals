4d6e51f5573ffe85d4a3f268b8c067d508e1d2b2
JamesMcGuigan <james.mcguigan.github@gmail.com>
2022-05-26 20:53:52 +0100

    next.config.js | configure less

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+// DOCS: https://www.npmjs.com/package/next-with-less
+const withPlugins = require("next-compose-plugins");
+const withLess    = require("next-with-less");
+const plugins = [
+  [withLess, {
+    lessLoaderOptions: {}
+  }]
+];
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: true
 }
 
-module.exports = nextConfig
+module.exports = withPlugins(plugins, nextConfig);
