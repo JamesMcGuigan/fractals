abce4dcb417ef9d4ced35c8294becdfbbfc14dcd
JamesMcGuigan <james.mcguigan.github@gmail.com>
2022-06-24 19:12:07 +0100

    nextjs | document custom-webpack-config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 // DOCS: https://www.npmjs.com/package/next-with-less
-const withPlugins = require("next-compose-plugins");
-const withLess    = require("next-with-less");
+const withPlugins    = require("next-compose-plugins");
+const withLess       = require("next-with-less");
+// const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin")
+// const path           = require("path");
+
 const plugins = [
   [withLess, {
     lessLoaderOptions: {}
@@ -9,7 +12,24 @@ const plugins = [
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+
+  // // BUG:  HookWebpackError: Cannot read properties of undefined (reading 'traceChild')
+  // // DOCS: https://nextjs.org/docs/api-reference/next.config.js/custom-webpack-config
+  // webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+  //   // Important: return the modified config
+  //   return {
+  //     ...config,
+  //     plugins: [
+  //       new WasmPackPlugin({
+  //         // crateDirectory: path.resolve(__dirname, "rust"),
+  //         crateDirectory: path.resolve(__dirname, "wasm-react-webpack-template"),
+  //         forceMode: "production",
+  //         // outDir: "public/wasm/",
+  //       })
+  //     ],
+  //   }
+  // }
 }
 
 module.exports = withPlugins(plugins, nextConfig);
