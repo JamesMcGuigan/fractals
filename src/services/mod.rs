07a5441d6d0cf4933154b8c60f2c22a280e6a481
JamesMcGuigan <james.mcguigan.github@gmail.com>
2022-06-28 18:46:49 +0100

    resize_service.rs + resize_service_js.rs | copy/paste code from internet

---
+++ src/services/mod.rs
@@ -0,0 +1,4 @@
+mod resize_service;
+mod resize_service_js;
+// pub use resize_service::ResizeService;
+pub use resize_service_js::ResizeServiceJs;
---
+++ src/services/resize_service_js.rs
@@ -0,0 +1,39 @@
+// Source: https://github.com/hgzimmerman/FullstackRustDemo/blob/master/frontend/boids/src/resize_service.rs
+
+use stdweb::{js, Value};
+use yew::Callback;
+
+pub struct ResizeServiceJs {}
+
+pub struct ResizeTask(Option<Value>);
+
+impl ResizeServiceJs {
+    pub fn new() -> ResizeServiceJs {
+        ResizeServiceJs {}
+    }
+
+    pub fn register(&mut self, callback: Callback<()>) -> ResizeTask {
+        let callback = move || {
+            callback.emit(());
+        };
+        let handle = js! {
+            var callback = @{callback};
+            var action = function() {
+                callback();
+            };
+            return window.addEventListener("resize", action);
+        };
+        ResizeTask(Some(handle))
+    }
+}
+
+impl Drop for ResizeTask {
+    fn drop(&mut self) {
+        let handle = self.0.take().expect("Resize task already empty.");
+        js! {
+            @(no_return)
+            var handle = @{handle};
+            handle.callback.drop();
+        }
+    }
+}
