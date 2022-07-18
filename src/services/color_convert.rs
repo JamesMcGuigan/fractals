36ee202f7569296d587fa2f4dc55d3c56323d4e0
JamesMcGuigan <james.mcguigan.github@gmail.com>
2022-07-18 23:47:43 +0100

    color_convert.rs | convert unit tests into doctests

--- src/services/color_convert.rs
+++ src/services/color_convert.rs
@@ -1,6 +1,22 @@
 use colors_transform::{Color, Hsl};
 use contracts::requires;
 
+/// Convert hsl to rgba u32
+/// ```
+/// # use fractals::services::color_convert::hsl_to_u32;
+/// # use fractals::services::colors::*;
+/// for (h, s, l, hex) in [
+///     (  0.,   0.,   0.,  BLACK),
+///     (  0.,   0., 100.,  WHITE),
+///     (  0.,   0.,  50.,  0x7F7F7FFF),  // GRAY
+///     (  0., 100.,  50.,  RED),
+///     (120., 100.,  50.,  GREEN),
+///     (240., 100.,  50.,  BLUE),
+/// ] {
+///     let result = hsl_to_u32(h as f32, s as f32, l as f32);
+///     assert_eq!(result, hex, "hsl_to_u32({}, {}, {}) | 0x{:08X} != 0x{:08X} RGBA", h, s, l, result, hex);
+/// }
+/// ```
 #[requires(h >=   0.0, "360. >= h >= 0.")]
 #[requires(h <= 360.0, "360. >= h >= 0.")]
 #[requires(s >=   0.0, "100. >= s >= 0.")]
@@ -13,6 +29,15 @@ pub fn hsl_to_u32(h: f32, s: f32, l: f32) -> u32 {
 }
 
 
+/// Convert a percentage value to grayscale rgba u32
+/// ```
+/// # use fractals::services::color_convert::grayscale_to_u32;
+/// assert_eq!(grayscale_to_u32(0.0),  0x000000FF);
+/// assert_eq!(grayscale_to_u32(0.25), 0x3F3F3FFF);
+/// assert_eq!(grayscale_to_u32(0.5),  0x7F7F7FFF);
+/// assert_eq!(grayscale_to_u32(0.75), 0xBFBFBFFF);
+/// assert_eq!(grayscale_to_u32(1.0),  0xFFFFFFFF);
+/// ```
 #[requires(percentage >= 0.0, "1.0 >= percentage >= 0.0")]
 #[requires(percentage <= 1.0, "1.0 >= percentage >= 0.0")]
 pub fn grayscale_to_u32(percentage: f32) -> u32 {
@@ -20,88 +45,16 @@ pub fn grayscale_to_u32(percentage: f32) -> u32 {
     rbga_to_u32(value, value, value,255)
 }
 
-#[allow(clippy::identity_op)]
+/// Convert rgba u8's into u32 hex
+/// ```
+/// # use fractals::services::color_convert::rbga_to_u32;
+/// # use fractals::services::colors::*;
+/// assert_eq!(rbga_to_u32(255,0,0,255),     RED  );
+/// assert_eq!(rbga_to_u32(0,255,0,255),     GREEN);
+/// assert_eq!(rbga_to_u32(0,0,255,255),     BLUE );
+/// assert_eq!(rbga_to_u32(255,255,255,255), WHITE);
+/// assert_eq!(rbga_to_u32(0,0,0,255),       BLACK);
+/// ```
 pub fn rbga_to_u32(r: u8, g: u8, b: u8, a: u8) -> u32 {
     ((r as u32) << 24) | ((g as u32) << 16) | ((b as u32) << 8) | ((a as u32) << 0)
-}
-
-
-#[cfg(test)]
-mod tests {
-    use crate::services::colors::*;
-
-    use super::*;
-
-    mod rbga_to_u32 {
-        use super::*;
-
-        #[test]
-        fn red() {
-            let result = rbga_to_u32(255 as u8, 0 as u8, 0 as u8, 255 as u8);
-            assert_eq!(result, RED, "{:X} != {:X}", result, RED);
-        }
-        #[test]
-        fn green() {
-            let result = rbga_to_u32(0 as u8, 255 as u8, 0 as u8, 255 as u8);
-            assert_eq!(result, GREEN, "{:X} != {:X}", result, GREEN);
-        }
-        #[test]
-        fn blue() {
-            let result = rbga_to_u32(0 as u8, 0 as u8, 255 as u8, 255 as u8);
-            assert_eq!(result, BLUE, "{:X} != {:X}", result, BLUE);
-        }
-        #[test]
-        fn black() {
-            let result = rbga_to_u32(0 as u8, 0 as u8, 0 as u8, 255 as u8);
-            assert_eq!(result, BLACK, "{:X} != {:X}", result, BLACK);
-        }
-        #[test]
-        fn white() {
-            let result = rbga_to_u32(255 as u8, 255 as u8, 255 as u8, 255 as u8);
-            assert_eq!(result, WHITE, "{:X} != {:X}", result, WHITE);
-        }
-    }
-
-    mod hsl_to_u32 {
-        use super::*;
-
-        #[test]
-        fn test_hsl_to_u32() {
-            let colormap = [
-                (  0.,   0.,   0.,  BLACK),
-                (  0.,   0., 100.,  WHITE),
-                (  0.,   0.,  50.,  0x7F7F7FFF),  // GRAY
-                (  0., 100.,  50.,  RED),
-                (120., 100.,  50.,  GREEN),
-                (240., 100.,  50.,  BLUE),
-            ];
-            for (h, s, l, hex) in colormap {
-                let result = hsl_to_u32(h as f32, s as f32, l as f32);
-                assert_eq!(result, hex,
-                           "hsl_to_u32({}, {}, {}) | 0x{:08X} != 0x{:08X} RGBA", h, s, l, result, hex);
-            }
-        }
-    }
-
-    mod grayscale_to_u32 {
-        use super::*;
-
-        #[test]
-        fn test_grayscale_to_u32() {
-            let colormap = [
-                (0.0,   BLACK),
-                (0.251, 0x404040FF),
-                (0.50,  0x7F7F7FFF),
-                (0.505, 0x808080FF),
-                (0.75,  0xBFBFBFFF),
-                (0.755, 0xC0C0C0FF),
-                (1.0,   WHITE),
-            ];
-            for (percentage, hex) in colormap {
-                let result = grayscale_to_u32(percentage);
-                assert_eq!(result, hex,
-                           "grayscale_to_u32({}) | 0x{:08X} != 0x{:08X} RGBA", percentage, result, hex);
-            }
-        }
-    }
 }
